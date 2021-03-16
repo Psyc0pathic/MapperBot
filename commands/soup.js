e99cbd7eda57081aadf0cb6a16fc8f0e4ba0ffdb
megamaz <raphael.mazuel@gmail.com>
2021-03-16 09:33:05 -0700

    soup

--- commands/soup.js
+++ commands/soup.js
@@ -3,8 +3,11 @@ module.exports = {
     name: 'soup',
     description: "soup",
     async run (client, message, args) {
-        if(!talkedRecently.has(message.author.id))
-            message.channel.send('soup\nhttps://cdn.discordapp.com/attachments/500326922509615105/821165696112656384/soop.mp4');
-        else message.channel.send(`**${message.author.tag}**, Wait 5 seconds.`)
+        if(message.channel.id == "500326922509615105")
+        {
+            // untested bcz idk JS
+            if(!talkedRecently.has(message.author.id)) message.channel.send('soup', {files:["./attachements/soop.mp4"]});
+            else message.channel.send(`**${message.author.tag}**, Wait 5 seconds.`)
+        }
     }
 }
