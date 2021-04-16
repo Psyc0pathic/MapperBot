d031e9a4673eb649976c5e68bbb0923b910884fe
Psyc0pathic <70185357+Psyc0pathic@users.noreply.github.com>
2021-04-16 08:03:35 -0700

    Revert "Fixed Lb command"

--- commands/lb.js
+++ commands/lb.js
@@ -6,58 +6,22 @@ module.exports = {
     description: "Check the sever's leaderboard",
 
     async run (client, message, args) {
-        let money = db.all().filter(item => item.ID.startsWith("money_"));
-        
+        let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data' })
+
         let content = "";
-        
-        let userToGet = message.mentions.users.first() || null;
 
-        let start = 0;
-        let end = 10;
-        if(userToGet != null)
-        {
-            
-            // get user's rank
-            for (let index = 0; index < money.length; index++) {
-                if (money[index].ID == `money_${message.guild.id}_${userToGet.id}`){
-                    start = index-5;
-                    if(start < 0) start = 0;
-                    else content += `...\n`;
-                    break
-                }
-                
-            }
+        for (let i = 0; i < money.length; i++){
+            let user = client.users.cache.get(money[i].ID.split('_')[2]).username
+
+            content += `${i+1}. ${user} - ${money[i].data} \n`;
+
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`${message.guild.name}'s Leaderboard`)
+            .setDescription(`${content}`)
+            .setColor("RANDOM")
+            .setTimestamp()
+
+            message.channel.send(embed);
         }
-        end = start+10;
-        if (end > money.length) end = money.length;
-        
-        for (let i = start; i < end; i++){
-
-            let username = message.guild.members.cache.get(money[i].ID.split("_")[2]);
-            if(username == undefined)
-            {
-                content += `${i+1}. Unknown Player - ${money[i].data} \n`;
-                continue;
-
-            }
-            let doBold = false;
-            if (userToGet != null)
-            {
-                doBold = (username.id == userToGet.id);
-                if (doBold) content += `**`
-            }
-            content += `${i+1}. ${username} - ${money[i].data} \n`;
-            if (userToGet != null && doBold) content += `**`
-
-
-        }
-        if (end != money.length) content += '...';
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`${message.guild.name}'s Leaderboard`)
-        .setDescription(`${content}`)
-        .setColor("RANDOM")
-        .setTimestamp()
-
-        message.channel.send(embed);
     }
 }
