4d87ea2edb49eb8a90a875a47b826fec30fc87af
megamaz <raphael.mazuel@gmail.com>
2021-04-27 13:45:19 -0700

    re-fixed help, lb, and transfer.

--- commands/lb.js
+++ commands/lb.js
@@ -8,7 +8,28 @@ module.exports = {
     async run (client, message, args) {
         let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data' })
 
-        let content = "";
+
+        let start = 0;
+        let end = 10;
+        if(userToGet != null)
+        {
+            
+            // get user's rank
+            for (let index = 0; index < money.length; index++) {
+                if(money[index].ID == `money_${message.guild.id}_${userToGet.id}`) {
+                    start = index-5;
+                    if(start < 0) start = 0;
+                    else content += `...\n`;
+                    break
+                }
+            }
+        }
+        end = start+5;
+        if (end > money.length) end = money.length;
+        
+        money.sort(function(first, second){
+            return second.data - first.data;
+        });
 
         for (let i = 0; i < money.length; i++){
             let user = client.users.cache.get(money[i].ID.split('_')[2]).username
